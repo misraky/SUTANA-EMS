72cb278a2225e79312d64ea86f396727c83c0757
Misrak Tsehayneh <misrak079@gmail.com>
2026-06-13 04:10:23 -0900

    Merged old and new notification logic

--- backend/src/api/routes/notification.routes.js
+++ backend/src/api/routes/notification.routes.js
@@ -6,15 +6,14 @@ const { authenticate } = require('../middleware/auth.middleware');
 // All notification routes require authentication
 router.use(authenticate);
 
+// --- YOUR OLD ROUTES ---
 router.get('/', NotificationController.getNotifications);
 router.put('/:id/read', NotificationController.markRead);
 router.put('/read-all', NotificationController.markAllRead);
-const notificationController = require('../controllers/notification.controller');
-const { authenticate } = require('../middleware/auth.middleware');
 
-router.use(authenticate);
-router.get('/', notificationController.getMyNotifications);
-router.patch('/mark-all-read', notificationController.markAllAsRead);
-router.patch('/:id/read', notificationController.markAsRead);
+// --- PARTNER'S NEW ROUTES ---
+router.get('/v2', NotificationController.getMyNotifications);
+router.patch('/v2/mark-all-read', NotificationController.markAllAsRead);
+router.patch('/v2/:id/read', NotificationController.markAsRead);
 
 module.exports = router;
